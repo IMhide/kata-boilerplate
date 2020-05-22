788297732df90e0a60fc46cfa6e1ba6d66ecd3d9
hide <sam.hide.ettahiri@gmail.com>
2020-05-22 12:31:54 +0200

    Deleting katas leftovers

--- spec/lib/mod_spec.rb
+++ spec/lib/mod_spec.rb
@@ -1,8 +1,9 @@
-describe BankOcr do
+describe Mod do
   it "is a module" do
-    expect(BankOcr).to be_a Module
+    expect(Mod).to be_a Module
   end
 
+  #should fail
   it "loads fixtures" do
     expect(fixture("use_case_1")).to match(/123456789\n\z/)
   end
